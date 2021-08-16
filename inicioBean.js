25cfa5de5f56724e7b25e1fcc27e85a0059041a0
Beatriz Medeiros <biamedcost@hotmail.com>
2021-08-16 01:45:50 -0300

    Adição da história de Bean e primeiras escolhas

---
+++ inicioBean.js
@@ -0,0 +1,16 @@
+var desicao;
+
+function escolha(){
+        prompt("O que você fará agora? \n -Vou ao bar encher a cara \n -Vou me arrumar para a droga do casamento");
+        while (true) {
+        if (desicao.toLowerCase() == "Vou ao bar encher a cara"){
+        break;
+        } else if 
+        (desicao.toLowerCase() == "Vou me arrumar para a droga do casamento"){
+            break;
+            } else {
+                alert("Insira uma opção válida!")
+            }
+        }
+    }
+

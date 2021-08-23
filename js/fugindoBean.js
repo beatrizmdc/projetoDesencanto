015671cbd9ece0c9cc192148237e0f00068a446e
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 21:13:07 -0300

    Update fugindoBean.js

--- js/fugindoBean.js
+++ js/fugindoBean.js
@@ -4,13 +4,13 @@ function faseQuatro(){
     escolha = prompt("O que você vai fazer agora? \n 1- Fugimos juntos, afinal ele é um Elfo muito fofo. \n 2- Largo ele pra trás, não gosto de Elfos. \n Escolha 1 ou 2");
         if (escolha == 1) {
             alert("Vocês fogem rumo à lugar nenhum, mas pelo menos têm companhia.");
-            window.location.href = "ultimaTelaBean.html";
+            window.location.href = "../html/ultimaTelaBean.html";
             return;
         } 
         
         if (escolha == 2) {
             alert("O karma chegou a você mais rápido do que pensava! Por não gostar de Elfos, assim que o deixou pra trás, foi capturada pelos cavaleiros do reino.")
-            window.location.href = "finalRuimBean.html";
+            window.location.href = "../html/finalRuimBean.html";
             return;
         } 
 

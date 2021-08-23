7e5fceed3ce2fae0e15d0ee23323194a0b70333b
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 21:16:38 -0300

    Update fugindoElfo.js

--- js/fugindoElfo.js
+++ js/fugindoElfo.js
@@ -4,13 +4,13 @@ function faseDois(){
     escolha = prompt("O que você vai fazer? \n 1- Me esconder debaixo do vestido da princesa \n 2- Aceitar o destino (que, no caso, é a morte) \n Escolha 1 ou 2");
         if (escolha == 1) {
             alert("Boa escolha, após cair da janela junto com a princesa, você agora está junto com ela nessa fuga!");
-            window.location.href = "ultimaTelaElfo.html";
+            window.location.href = "../html/ultimaTelaElfo.html";
             return;
         } 
         
         if (escolha == 2) {
             alert("Nadou, nadou, para morrer na praia? Era melhor ter continuado a ser um Elfo feliz.")
-            window.location.href = "finalRuimBean.html";
+            window.location.href = "../html/finalRuimBean.html";
             return;
         } 
 

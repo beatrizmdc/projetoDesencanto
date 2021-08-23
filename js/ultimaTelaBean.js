c20ddb8e28659e030abc1b98543093f4f0aafd84
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 21:15:07 -0300

    Update ultimaTelaBean.js

--- js/ultimaTelaBean.js
+++ js/ultimaTelaBean.js
@@ -4,13 +4,13 @@ function faseCinco(){
     escolha = prompt("Qual sua última escolha? \n 1- Que se dane, vou pular. \n 2- Aceito me casar com o irmão de meu falecido noivo. \n Escolha 1 ou 2");
         if (escolha == 1) {
             alert("Bom, você caiu do precipício. Talvez Luci ou Elfo tenham algum poder mágico para te ajudarem a sair dessa...");
-            window.location.href = "finalBomBean.html";
+            window.location.href = "../html/finalBomBean.html";
             return;
         } 
         
         if (escolha == 2) {
             alert("Você não caiu do precipício, mas era uma escolha melhor do que se casar com o irmão do seu falecido noivo. Que insensível da sua parte!")
-            window.location.href = "finalRuimBean.html";
+            window.location.href = "../html/finalRuimBean.html";
             return;
         } 
 
@@ -19,3 +19,4 @@ function faseCinco(){
             return;
             }
         }
+

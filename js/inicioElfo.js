583fd2d4e27ee1c53e0f551bab00a757505c67ac
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 22:48:59 -0300

    Update inicioElfo.js

--- js/inicioElfo.js
+++ js/inicioElfo.js
@@ -4,13 +4,13 @@ function faseUm(){
     escolha = prompt("O que você vai fazer? \n 1- Vou me jogar no mundão. \n 2- Vou seguir com a minha vida feliz. \n Escolha 1 ou 2");
         if (escolha == 1) {
             alert("Rumo a Terra dos Sonhos!");
-            window.location.href = "../html/fugindoElfo.html";
+            window.location.href ="../html/fugindoElfo.html";
             return;
         } 
         
         if (escolha == 2) {
             alert("Boa sorte nessa sua vidinha medíocre.")
-            window.location.href = "../html/finalRuimBean.html";
+            window.location.href ="../html/finalRuimBean.html";
             return;
         } 
 

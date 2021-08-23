9860b97d372e9298761649139772b5950ec3fc83
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 21:16:16 -0300

    Update inicioLuci.js

--- js/inicioLuci.js
+++ js/inicioLuci.js
@@ -4,13 +4,13 @@ function faseUm(){
     escolha = prompt("Qual sua escolha? \n 1- Vou ajudar essa coitada, porque de pobre não tem nada. \n 2- Ela que lute, não tenho nada a ver com isso. \n Escolha 1 ou 2");
         if (escolha == 1) {
             alert("Vocês planejam uma fuga no meio do casamento, porque são afrontosos.");
-            window.location.href = "fugindoLuci.html";
+            window.location.href = "../html/fugindoLuci.html";
             return;
         } 
         
         if (escolha == 2) {
             alert("Você só tem uma função no mundo e ainda se recusa a fazê-la? Provavelmente você precisa bater um papinho com o verdadeiro dono do inferno...")
-            window.location.href = "finalRuimBean.html";
+            window.location.href = "../html/finalRuimBean.html";
             return;
         } 
 

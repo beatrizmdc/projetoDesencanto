94a446f6cba55ac8b1c39a14fad4d40699f23fea
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 21:10:56 -0300

    Update finalRuimBean.js

--- js/finalRuimBean.js
+++ js/finalRuimBean.js
@@ -2,7 +2,7 @@ function respostaJogador(){
     escolha = prompt("Digite 1 para jogar novamente!");
         
         if (escolha == 1) {
-            window.location.href = "personagens.html";
+            window.location.href = "../index.html";
             return;
         } 
 

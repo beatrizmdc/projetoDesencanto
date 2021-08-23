2d0b32c6912db2cbe0fede38c9b028991378d49a
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 21:13:40 -0300

    Update fugindoLuci.js

--- js/fugindoLuci.js
+++ js/fugindoLuci.js
@@ -4,13 +4,13 @@ function faseDois(){
     escolha = prompt("Qual sua escolha? \n 1- Bora chamar o Elfo pro bonde. \n 2- Não quero saber de Elfos no meu squad. \n Escolha 1 ou 2");
         if (escolha == 1) {
             alert("Agora vocês são um trio inseparável. Mal se conhecem, mas já se consideram pacas!");
-            window.location.href = "ultimaTelaLuci.html";
+            window.location.href = "../html/ultimaTelaLuci.html";
             return;
         } 
         
         if (escolha == 2) {
             alert("Não sei se você percebeu, mas quem manda aqui é a Bean. Se não aceitar o Elfo, quem tá fora é você!")
-            window.location.href = "finalRuimBean.html";
+            window.location.href = "../html/finalRuimBean.html";
             return;
         } 
 

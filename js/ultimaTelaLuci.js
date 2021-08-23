e1e8bce275d1a927db4247abd1a33032081a134c
Beatriz Medeiros Costa <88167061+beatrizmdc@users.noreply.github.com>
2021-08-22 21:15:52 -0300

    Update ultimaTelaLuci.js

--- js/ultimaTelaLuci.js
+++ js/ultimaTelaLuci.js
@@ -4,13 +4,13 @@ function faseFinal(){
     escolha = prompt("Qual sua escolha? \n 1- Incentivar a pular do penhasco. \n 2- Incentivar a casar com o irmão do falecido noivo, não quero morrer. \n Escolha 1 ou 2");
         if (escolha == 1) {
             alert("Vocês cairam do precipício. Será que você tem algum poder mágico oculto para sair dessa situação?");
-            window.location.href = "finalBomBean.html";
+            window.location.href = "../html/finalBomBean.html";
             return;
         } 
         
         if (escolha == 2) {
             alert("Bom, a sua única função na terra era ajudar a Bean a fugir do casamento. Você já não tem nenhuma utilidade!")
-            window.location.href = "finalRuimBean.html";
+            window.location.href = "../html/finalRuimBean.html";
             return;
         } 
 
